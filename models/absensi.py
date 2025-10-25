e75c5e1cabfacafdb20eddb71f545355b858feae
ellikhsan <134936500+ellikhsan@users.noreply.github.com>
2025-10-25 17:38:59 +0700

    Delete models directory

--- models/absensi.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, DateTime
-from . import Base
-
-class Absensi(Base):
-    __tablename__ = "absensi"
-
-    id = Column(Integer, primary_key=True, index=True)
-    siswa_id = Column(Integer, nullable=False)
-    waktu = Column(DateTime, nullable=False)
--- models/siswa.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from . import Base
-
-class Siswa(Base):
-    __tablename__ = "siswa"
-
-    id = Column(Integer, primary_key=True, index=True)
-    nama = Column(String, nullable=False)
-    nis = Column(String, unique=True, index=True)
